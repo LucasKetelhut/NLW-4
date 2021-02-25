5b0bcc7756c5e7ec411fbf69a8a0a84faa769bea
Lucas Ketelhut <lucas-kmk@hotmail.com>
2021-02-24 23:54:13 -0300

    Terceira aula, adicionando context

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,17 +1,23 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export function ExperienceBar() {
+    const { currentExp, expToNextLevel } = useContext(ChallengesContext);
+
+    const percentToNextLevel = Math.round(currentExp * 100) / expToNextLevel;
+
     return (
         <header className={styles.expBar}>
             <span>0 xp</span>
             <div>
-                <div style={{ width: '50%' }}/>
+                <div style={{ width: `${percentToNextLevel}` }}/>
 
-                <span className={styles.currentExp} style={{ left: '50%' }}>
-                    300 xp
+                <span className={styles.currentExp} style={{ left: `${percentToNextLevel}` }}>
+                    {currentExp} xp
                 </span>
             </div>
-            <span>600 xp</span>
+            <span>{expToNextLevel} xp</span>
         </header>
     );
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext); 
+
     return (
         <div className={styles.profileContainer} >
             <img src="https://github.com/LucasKetelhut.png" alt="Lucas Ketelhut" />
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Lucas Ketelhut</strong>
                 <p>
                     <img src="icons/level.svg" alt="Ícone de level"/>
-                    Level 3
+                    Level {level}
                 </p>
             </div>
         </div>
